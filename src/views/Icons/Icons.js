1f1328e41dd51c689a571a69c0b59582506a9324
Taofik <bidemi64@gmail.com>
2020-12-26 21:38:14 +0100

    made the users component

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -1,65 +1,37 @@
-/*eslint-disable*/
-import React from "react";
-// @material-ui/core components
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Hidden from "@material-ui/core/Hidden";
-// core components
-import GridItem from "components/Grid/GridItem.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import Card from "components/Card/Card.js";
-import CardHeader from "components/Card/CardHeader.js";
-import CardBody from "components/Card/CardBody.js";
+import { Box, Container } from '@material-ui/core';
+// import styles from "assets/jss/material-dashboard-react/views/iconsStyle.js";
+import Page from '../../components/Page/Page';
+import Toolbar from '../../components/Page/Toolbar'
+// import Results from '../../components/Results';
+import data from '../../components/Page/data';
+import Results from '../../components/Page/Results';
 
-import styles from "assets/jss/material-dashboard-react/views/iconsStyle.js";
-
-const useStyles = makeStyles(styles);
-
+// const useStyles = makeStyles(styles);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
 export default function Icons() {
   const classes = useStyles();
+  const [customers] = useState(data);
+
   return (
-    <GridContainer>
-      {/* <GridItem xs={12} sm={12} md={12}>
-        <Card plain>
-          <CardHeader plain color="primary">
-            <h4 className={classes.cardTitleWhite}>Material Design Icons</h4>
-            <p className={classes.cardCategoryWhite}>
-              Handcrafted by our friends from{" "}
-              <a
-                href="https://design.google.com/icons/?ref=creativetime"
-                target="_blank"
-              >
-                Google
-              </a>
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Hidden only={["sm", "xs"]}>
-              <iframe
-                className={classes.iframe}
-                src="https://material.io/icons/"
-                title="Icons iframe"
-              >
-                <p>Your browser does not support iframes.</p>
-              </iframe>
-            </Hidden>
-            <Hidden only={["lg", "md"]}>
-              <GridItem xs={12} sm={12} md={6}>
-                <h5>
-                  The icons are visible on Desktop mode inside an iframe. Since
-                  the iframe is not working on Mobile and Tablets please visit
-                  the icons on their original page on Google. Check the
-                  <a
-                    href="https://design.google.com/icons/?ref=creativetime"
-                    target="_blank"
-                  >
-                    Material Icons
-                  </a>
-                </h5>
-              </GridItem>
-            </Hidden>
-          </CardBody>
-        </Card>
-      </GridItem> */}
-    </GridContainer>
+    <Page
+      className={classes.root}
+      title="Customers"
+    >
+      <Container maxWidth={false}>
+        <Toolbar />
+        <Box mt={3}>
+          <Results customers={customers} />
+        </Box>
+      </Container>
+    </Page>
   );
 }
